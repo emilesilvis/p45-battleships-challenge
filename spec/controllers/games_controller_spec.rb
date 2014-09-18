a2fc87647d2353e0d26a9a0e1e8525973ec9dd6d
Emile Silvis <emile@silvis.co.za>
2014-09-18 14:36:54 +0200

    Refactor GamesController spec

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,14 +1,31 @@
 require 'spec_helper'
 
 describe GamesController do
+
   describe "GET #index" do
     before do
       @game = Game.create(session_id: 123, player_name: 'Test Player', player_email: 'player@test.com', player_board: GameEngine::Board.new(10, 10), opponent_board: GameEngine::Board.new(10, 10))
+      get :index
     end
 
-    it "shows games" do
-      get :index
+    it "has a collection of games" do
       expect(assigns(:games)).to include @game
+    end
+
+    it "renders index view" do
+      expect(response).to render_template( 'index' )
+    end
+  end
+
+  describe "GET #new" do
+    before do
+      get :new
+    end
+    it "makes a new game" do
+      expect(assigns(:game)).to be_a Game
+    end
+    it "renders new view" do
+      expect(response).to render_template( 'new' )
     end
   end
 
@@ -19,13 +36,11 @@ describe GamesController do
       end
 
       it "is not valid" do
-        game = assigns(:game)
         expect(assigns(:game)).to_not be_valid
       end
 
-      it "redirects to #new" do
-        game = assigns(:game)
-        expect(response).to render_template( "new" )
+      it "redirects to new" do
+        expect(response).to render_template( 'new' )
       end
     end
 
@@ -35,13 +50,11 @@ describe GamesController do
       end
 
       it "is valid" do
-        game = assigns(:game)
         expect(assigns(:game)).to be_valid
       end
 
-      it "redirects to #show" do
-        game = assigns(:game)
-        expect(response).to redirect_to( game_path(game) )
+      it "redirects to show" do
+        expect(response).to redirect_to( game_path(assigns(:game)) )
       end
     end
   end
@@ -49,11 +62,38 @@ describe GamesController do
   describe "GET #show" do
     before do
       @game = Game.create(session_id: 123, player_name: 'Test Player', player_email: 'player@test.com', player_board: GameEngine::Board.new(10, 10), opponent_board: GameEngine::Board.new(10, 10))
+      get :show, {id: @game.id}
     end
 
-    it "shows game" do
-      get :show, {id: @game.id}
+    it "has a game" do
       expect(assigns(:game)).to_not be_nil
+    end
+
+    it "has a player grid" do
+      expect(assigns(:player_grid)).to_not be_nil
+    end
+
+    it "has an opponent_grid" do
+      expect(assigns(:opponent_grid)).to_not be_nil
+    end
+
+    it "renders show view" do
+      expect(response).to render_template( 'show' )
+    end
+  end
+
+  describe "PATCH #update" do
+    before do
+      @game = Game.create(session_id: 123, player_name: 'Test Player', player_email: 'player@test.com', player_board: GameEngine::Board.new(10, 10), opponent_board: GameEngine::Board.new(10, 10))
+      patch :update, {id: @game.id, x: 1, y: 1}
+    end
+
+    it "has a game" do
+      expect(assigns(:game)).to_not be_nil
+    end
+
+    it "redirects to show" do
+      expect(response).to redirect_to( game_path(@game.id) )
     end
   end
 
@@ -61,10 +101,19 @@ describe GamesController do
     before do
       post :create, game: {player_name: 'Test Player', player_email: 'player@test.com'}
       @game = assigns(:game)
-    end
-    it "is destroyed" do
       delete :destroy, {id: @game.id}
+    end
+
+    it "destroys game" do
       expect{ Game.find(@game.id) }.to raise_error("Couldn't find Game with id=#{@game.id}")
+    end
+
+    it "creates a flash message" do
+      expect(flash[:success]).to eq "Game deleted!"
+    end
+
+    it "redirects to index" do
+      expect(response).to redirect_to( games_path )
     end
   end
 end
